8e89cc1bd1259b7ebc060fd5ebadc32d6365b3ea
Subhajit Mitra <143322078+SubhajitMitra-GH@users.noreply.github.com>
2025-11-07 19:28:17 +0530

    Update build script to enable EPEL repository
    Added instructions to enable the EPEL repository before installing ffmpeg.

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Amazon Linux (which Vercel uses) uses yum, not apt-get
+# 1. Enable the EPEL (Extra Packages) repository
+amazon-linux-extras install epel -y
+
+# 2. Now that the repository is added, install ffmpeg
 yum install -y ffmpeg
